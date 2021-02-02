34f46a15edee3ea3b4b8476a863eecbd4bd153ae
evancf <ecfricke@iastate.edu>
2021-02-02 13:03:32 -0800

    Adding ps1 instructions and submission template

---
+++ ps1_instructions.R
@@ -0,0 +1,44 @@
+# EBIO 338/538 Problem Set 1
+
+#1 Import the problem set data file on gibbon foods and name the object food
+
+#2 What are the dimensions of the data?
+
+#3 What are the variable names?
+
+#4 What type of object is food and what class are the variables within it?
+
+#5 What is the mean fruiting frequency?
+
+#6 What is the mean stem density?
+
+#7 Extract all observations with one or more use into an object named eaten
+
+#8 What percent of genera were eaten by gibbons?
+
+#9 Add a new column to eaten called Form_Crop that contains information on form and cropsize separated by an underscore "_"
+
+#10 Display the first 5 lines of eaten.
+
+#11 How many unique combinations of FormCrop in eaten are there?
+
+#12 In the object eaten, convert Form "Tree" to "T" and "Vine" to "V"
+
+#13 Is the updated Form in eaten a factor? If so, make it a character string. If not, make it a factor.
+
+#14 Extract all observations from object food that have a crop size of II. Call the object CSII.
+
+#15 Create a list called mylist that contains the objects food, eaten, FormCrop, CSII, and the letter w. 
+
+#16 Name each element in mylist using the input object names
+
+#17 Extract the 4th element of the 3rd element in mylist using two different ways
+
+#18 Extract the observations of food that have DBH values greater than 10 but less than 55. How many observations meet these criteria?
+
+#19 Generate a TRUE/FALSE vector indicating which rows in object food have the value "Tree" for Form.
+
+#20 Using the object food, calculate total basal area, which is the product of stem density and DBH
+
+
+
